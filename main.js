1e18663181aaa5d93df92a830194bf97ec294107
yuna <yuna744@gmail.com>
2024-01-26 12:16:32 +0900

    0126 파라미터에서 타입을 optional이나 유니온타입을 지정하더라도 없을 수도 있는 값을 코드 안쪽에서 예외처리하지 않으면 컴파일 에러발생

--- main.js
+++ main.js
@@ -19,11 +19,96 @@ const plus = (n1, n2) => {
     console.log(n2);
 };
 plus(1, 2, 3, 5);
-let student1 = {
-    name: "Emily",
-    age: 30,
-    isFemale: true,
+// 메모리 할당
+//type 특정 커스터마이징된 자료형을 새로 선언할때
+//type vs interface차이점
+//interface는 객체에만 적용가능
+//type은 자료형 상관없이 모두 적용가능
+// type Grade = "A" | "B" | "C" | "D" | "F";
+/*
+interface Student {
+  name: string;
+  age: number;
+  readonly isFemale: boolean; //특정 property를 수정불가하게 읽기 전용으로 지정
+  // address?: string; //해당 property를 선택사항으로 지정
+  score: Grade;
+}
+*/
+// 인터페이스 객체를 타입 형식으로 만든 것
+//Interface 대신 type으로도 객체타입 선언가능
+// type Student = {
+//   name: string;
+//   age: number;
+//   readonly isFemale: boolean; //특정 property를 수정불가하게 읽기 전용으로 지정
+//   // address?: string; //해당 property를 선택사항으로 지정
+//   score: Grade;
+// };
+// let student1: Student = {
+//   name: "David",
+//   age: 20,
+//   isFemale: false,
+//   // address: "seoul",
+//   score: "F",
+// };
+// student1.score = "F";
+//interface는 객체의 property 확장에 따른 구조변경이 편함(추가할때)
+// interface Student {
+//   name: string;
+//   age: number;
+// }
+// interface Student {
+//   name: string;
+//   age: number;
+//   isFemale: boolean;
+// }
+// let student1: Student = {
+//   name: "David",
+//   age: 30,
+//   isFemale: false,
+// };
+// type
+// type Student = {
+//   name: string;
+//   age: number;
+// };
+// type Student = {
+//   name: string;
+//   age: number;
+//   isFemale: boolean;
+// };
+// let student1: Student = {
+//   name: "David",
+//   age: 30,
+//   isFemale: false,
+// };
+//type 주로 쓰는 경우
+//1. 기존 객체타입의 property의 추가가 아닌, 서로 다른 객체를 합쳐서 새로운 타입을 만들때
+//2. 객체가 아닌 일반 자료형인데
+// interface StudentA {
+//   name: string;
+//   age: number;
+// }
+// interface StudentB {
+//   name: string;
+//   isFemale: boolean;
+// }
+// //type이용해서 두개의 인터페이스에서 Intersection으로 &&로 합쳐서 새로타입지정
+// type StudentTotal = StudentA & StudentB;
+// let student1: StudentTotal = {
+//   name: "Emily",
+//   age: 30,
+//   isFemale: true,
+// };
+// //객체는 아니지만 특정 커스터마이징된 타입을 재활용해야 될떄 type으로 지정
+// type commonType = string | null;
+// const test = (info: commonType) => {
+//   console.log(info);
+// };
+//특정 파라미터값을 optional처리시 type의 오류는 넘어갈 수 있지만
+//optional처리한 값을 코드 내부적으로 예외처리하지 않으면 에러가 발생
+const test = (n1, n2, n3) => {
+    //const result = n1 + n2 + n3;
+    const result = n3 === undefined ? n1 + n2 : n1 + n2 + n3;
+    return result;
 };
-const test = (info) => {
-    console.log(info);
-};
+console.log(test(1, 2));
