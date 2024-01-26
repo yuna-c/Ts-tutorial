1281bd370165db2d31eb3200e4d35fa5458e6eaf
yuna <yuna744@gmail.com>
2024-01-26 16:01:21 +0900

    0126 extends 인터페이스 프롭 확장

--- main.ts
+++ main.ts
@@ -272,28 +272,54 @@ interface Student {
 //특정 상황에서만 다른 interface의 기능을 빌려와서 임시로 타입을 확장해서 호출하고 싶을때 사용
 //굳이 interface를 고치긴 번거롭고 확장해야 되는 기능이 복잡할때
 
-interface Student {
+// interface Student {
+//   name: string;
+//   age: number;
+// }
+
+// interface Abroad {
+//   nation: string;
+//   period: number;
+// }
+
+// const fetchData = <t>(info: t[]) => {
+//   console.log(info);
+// };
+
+// //특정 interface형식으로 배열값이 전달될때 (DB, API data)
+// const info1 = [
+//   { name: "David", age: 30, period: 40 },
+//   { name: "David", age: 30, period: 40 },
+//   { name: "David", age: 30, period: 40 },
+// ];
+
+// //generic으로 인터페이스 활용하는 사례
+// //범용적으로 배열로 묶여있는 객체데이터를 활용하는 (Component)
+// //특정 데이터 카테고리 외의 값이 전달시 에러나게 할때 Generict 특정 데이터의 인터페이스 전달
+// fetchData<Student>(info1);
+
+interface Post {
+  tit: string;
+  con: string;
+}
+interface User {
   name: string;
   age: number;
 }
 
-interface Abroad {
-  nation: string;
-  period: number;
-}
+/*
+const addPost = (data: Post) => {
+  console.log(data);
+};
+*/
 
-const fetchData = <t>(info: t[]) => {
-  console.log(info);
+// const addPost = <t>(data: t) => {
+//   console.log(data);
+// };
+// addPost<Post>({ tit: "a", con: "b"});
+
+const addPost = <t extends User>(data: t) => {
+  console.log(data);
 };
 
-//특정 interface형식으로 배열값이 전달될때 (DB, API data)
-const info1 = [
-  { name: "David", age: 30, period: 40 },
-  { name: "David", age: 30, period: 40 },
-  { name: "David", age: 30, period: 40 },
-];
-
-//generic으로 인터페이스 활용하는 사례
-//범용적으로 배열로 묶여있는 객체데이터를 활용하는 (Component)
-//특정 데이터 카테고리 외의 값이 전달시 에러나게 할때 Generict 특정 데이터의 인터페이스 전달
-fetchData<Student>(info1);
+addPost({ tit: "a", con: "b", name: "aa", age: 10 });
